34e7d59729e67c4681d97e46156511e223bf7e02
Rad <rad@radski.eu>
2022-09-23 22:26:05 +0200

    Fixed a bug of wrongly copying full resolution

--- src/app/components/sticker/sticker.component.ts
+++ src/app/components/sticker/sticker.component.ts
@@ -94,6 +94,7 @@ export class StickerComponent implements OnInit {
   async copyToClipboard(text: string) {
     if (this.size === '0') {
       navigator.clipboard.writeText(text);
+      return
     }
     navigator.clipboard.writeText(`${text.slice(0, -4)}_${this.size}.png`);
   }
