f2813770f708cefb5ca29b96629562eee31a3b12
farfelu <git@farfelu.eu>
2022-10-01 12:36:58 +0000

    docker run with interactive shell

--- downloader/download.sh
+++ downloader/download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t sticker-downloader .
-docker run -v $PWD/stickers:/app/stickers --rm sticker-downloader "$@"
+docker run -it -v $PWD/stickers:/app/stickers --rm sticker-downloader "$@"
